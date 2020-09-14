bf93cf923942871ab1a242fc463a335d1211d909
Carlos Castillo <closcastillo95@gmail.com>
2020-09-13 21:30:49 -0700

    adding project cards to the website

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +18,58 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+        <h1>Projects</h1>
+        <section className="card-list">
+          <article className="card">
+              <header>
+                <h1>Pathfinder Interactive</h1>
+                <p>
+                  An interactive pathfinding website that utilizes path 
+                  finding algorithms to find the most optimal path between two points
+                </p>
+              </header>
+              <div className="project-links">
+                <a href="https://pathfinder-interactive.herokuapp.com/" target="_blank" rel="noopener noreferrer"><i className="fab fa-github-square"></i></a>
+                <a href="https://pathfinder-interactive.herokuapp.com/" target="_blank" rel="noopener noreferrer"><img src="https://raw.githubusercontent.com/AGuyNamedC-Los/pathfinder_interactive/master/path.png" alt="pathfinder interactive"></img></a>
+              </div>
+              <h2>Using</h2>
+              <div className="tools">
+                <span>Javascript</span>
+                <span>Heroku</span>
+                <span>HTML5</span>
+                <span>CSS</span>
+              </div>
+              <h2>Completion: 80%</h2>
+              <div className="progress" data-value="80%">
+                <div className="progress-value"></div>
+              </div>
+          </article>
+          <article className="card">
+              <header>
+                <h1>Portfolio Website v2</h1>
+                <p>
+                  Second iteration of my personal portfolio website containing links to all 
+                  my work, criteria, and social media
+                </p>
+              </header>
+              <div className="project-links">
+                <a href="https://pathfinder-interactive.herokuapp.com/" target="_blank" rel="noopener noreferrer"><i className="fab fa-github-square"></i></a>
+                <a href="https://pathfinder-interactive.herokuapp.com/" target="_blank" rel="noopener noreferrer"><img src="https://raw.githubusercontent.com/AGuyNamedC-Los/pathfinder_interactive/master/path.png" alt="pathfinder interactive"></img></a>
+              </div>
+              <h2>Using</h2>
+              <div className="tools">
+                <span>Javascript</span>
+                <span>Heroku</span>
+                <span>HTML5</span>
+                <span>CSS</span>
+              </div>
+              <h2>Completion: 80%</h2>
+              <div className="progress" data-value="80%">
+                <div className="progress-value"></div>
+              </div>
+          </article>
+        </section>
     </div>
   );
 }
